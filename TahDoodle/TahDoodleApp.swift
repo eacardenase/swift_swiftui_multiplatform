48aaeee9f15ba5dcb5fb4eb72a9328ca6edb0847
Edwin Cardenas <ecardenas@lwolf.com>
2025-05-16 09:17:46 -0500

    Initial Commit

---
+++ TahDoodle/TahDoodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TahDoodleApp.swift
+//  TahDoodle
+//
+//  Created by Edwin Cardenas on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct TahDoodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
